bce8048ebd858d79825fa1326ea7d3c2bdbeca55
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-04-25 15:55:33 +0700

    membuat looping while untuk menyisipkan node baru ke link list

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -30,3 +30,21 @@ void addNote() {
         START = nodeBaru;
         return;
     }
+    Node* previous = START;
+Node* current = START;
+
+while ((current != NULL) && (nim >= current->noMhs))
+{
+    if (nim == current->noMhs)
+    {
+        cout << "NIM sudah ada" << endl;
+        return;
+    }
+    previous = current;
+    current = current->next;
+}
+
+    nodeBaru->next = current;
+    previous->next = nodeBaru;
+}
+
